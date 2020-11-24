7895747ae30d40d6541751ff1737d7ab44cf3cc7
Beatriz Alice <beatrizalice00@gmail.com>
2020-11-24 00:06:18 -0300

    add carrossel no home

--- melivrando/lib/app/home/home.dart
+++ melivrando/lib/app/home/home.dart
@@ -28,6 +28,121 @@ class _HomePageState extends State<HomePage> {
         ],
       ),
       drawer: DrawerHome(),
+      body: Container(
+        child: Column(
+          children: [
+            Container(
+              height: 200,
+              width: double.infinity,
+              child: Expanded(child: ListViewHome()),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ListViewHome extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      scrollDirection: Axis.horizontal,
+      children: [
+        Container(
+          width: 360,
+          color: Colors.blue,
+          child: News(),
+        ),
+        Container(
+          width: 360,
+          color: Colors.amber,
+          child: Classic(),
+        ),
+        Container(
+          width: 360,
+          color: Colors.deepPurple,
+          child: Sugestions(),
+        ),
+      ],
+    );
+  }
+}
+
+class News extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Text(
+            'Novidades!',
+            style: TextStyle(
+                color: Colors.white54, fontSize: 55, fontFamily: 'ComicDylans'),
+          ),
+          Icon(
+            Icons.fiber_new_rounded,
+            size: 55,
+            color: Colors.white54,
+          ),
+        ],
+      ),
+      onTap: () {
+        //PÁGINAS DE NOVIDADES AQUI
+      },
+    );
+  }
+}
+
+class Classic extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Text(
+            'Clássicos',
+            style: TextStyle(
+                color: Colors.white54, fontSize: 55, fontFamily: 'ComicDylans'),
+          ),
+          Icon(
+            Icons.book_rounded,
+            size: 55,
+            color: Colors.white54,
+          ),
+        ],
+      ),
+      onTap: () {
+        //PÁGINAS DE NOVIDADES AQUI
+      },
+    );
+  }
+}
+
+class Sugestions extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Text(
+            'Sugestões',
+            style: TextStyle(
+                color: Colors.white54, fontSize: 55, fontFamily: 'ComicDylans'),
+          ),
+          Icon(
+            Icons.alternate_email_sharp,
+            size: 55,
+            color: Colors.white54,
+          ),
+        ],
+      ),
+      onTap: () {
+        //PÁGINAS DE NOVIDADES AQUI
+      },
     );
   }
 }
